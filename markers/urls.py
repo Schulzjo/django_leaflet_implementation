ab99815bbc191c474c7b05c714d3e557d06acb67
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-13 22:25:32 +0100

    add create/delete view

--- markers/urls.py
+++ markers/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from markers.views import markers_map_view
+from markers.views import markers_map_view, markers_create_view, markers_delete_view
 
 app_name = "markers"
 
 urlpatterns = [
     path("map/", markers_map_view),
+    path("map/create/", markers_create_view, name="create"),
+    path("map/delete/<int:pk>", markers_delete_view, name="delete"),
 ]
-
