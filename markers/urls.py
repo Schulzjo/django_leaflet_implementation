8d7f5b3664ebe33f223f676edf5c95874e471e95
schulzjo <schulzeeverding.johannes@gmail.com>
2023-03-16 17:31:54 +0100

    refactor markers_map_view to class based view

--- markers/urls.py
+++ markers/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from markers.views import markers_map_view, markers_create_view, markers_delete_view
+from markers.views import markers_create_view, markers_delete_view, MarkersView
 
 app_name = "markers"
 
 urlpatterns = [
-    path("map/", markers_map_view),
+    path("map/", MarkersView.as_view(), name="map"),
     path("map/create/", markers_create_view, name="create"),
     path("map/delete/<int:pk>", markers_delete_view, name="delete"),
 ]
--- markers/views.py
+++ markers/views.py
@@ -3,15 +3,21 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView
 
 from markers.forms import MarkerForm
 from markers.models import Marker
 
 
-def markers_map_view(request):
-    markers = Marker.objects.all()
-    add_marker_form = MarkerForm()
-    return render(request, 'map.html', {'markers': markers, "add_marker_form": add_marker_form})
+class MarkersView(ListView):
+    template_name = 'map.html'
+    model = Marker
+    context_object_name = 'markers'  # used in the HTML template to loop through and list contacts
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_marker_form'] = MarkerForm()
+        return context
 
 
 @require_http_methods(["POST"])
@@ -33,3 +39,4 @@ def markers_delete_view(request, pk):
     res = HttpResponse(status=200)
     res.headers["HX-Trigger"] = json.dumps({"delete_marker_from_map": pk})
     return res
+
